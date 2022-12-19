cfe348cb7745e5a9e5ad3746d72431404dab9264
Alex Freer <chasealex6@hotmail.com>
2022-12-19 11:46:58 -0500

    Update imageClassification.py

--- imageClassification.py
+++ imageClassification.py
@@ -1,12 +1,16 @@
 import glob
 import tensorflow as tf
 
+from tensorflow import keras
+from keras import layers
+from keras.models import Sequential
+
 batch_size = 32
 img_height = 369
 img_width = 496
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  "/dataset",
+  "dataset",
   validation_split=0.2,
   subset="training",
   seed=123,
@@ -14,11 +18,47 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
   batch_size=batch_size)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  "/dataset",
+  "dataset",
   validation_split=0.2,
   subset="validation",
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-print(train_ds.class_names)
+class_names = train_ds.class_names
+print(class_names)
+
+# Setup prefetch
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+
+# making the model
+num_classes = len(class_names)
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+model.save('model')
