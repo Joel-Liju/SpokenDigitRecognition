b9c8e9bdb8d46edcfde3e4df80c02f78b8d4f471
Dare <joelliju10@gmail.com>
2022-12-19 11:02:09 -0500

    splits data properly

---
+++ datasplitter.py
@@ -0,0 +1,16 @@
+import glob
+import os
+import shutil
+dataFolder = 'dataset'
+fNames = glob.glob(dataFolder+'/*.png')
+for name in fNames:
+    print(name)
+    directory,file = name.split('\\')
+    if not os.path.isdir(directory+'/'+file[0]):
+        os.mkdir(directory+'/'+file[0])
+    shutil.move(name,directory+'/'+file[0]+'/'+file)
+print("done first part")
+fNames = glob.glob(dataFolder+'/*.wav')
+for name in fNames:
+    os.remove(name)
+print("done second portion")
