c5b8e70222d76edf4dacfbd83526adb1cd39ceb4
Alex Freer <chasealex6@hotmail.com>
2022-12-29 13:33:17 -0500

    Update imageClassification.py

--- imageClassification.py
+++ imageClassification.py
@@ -34,8 +34,9 @@ model = Sequential([
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Conv2D(128, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
   layers.Flatten(),
-  layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
 
@@ -45,11 +46,13 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs=20
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 history = model.fit(
   train_ds,
   validation_data=val_ds,
-  epochs=epochs
+  epochs=5
 )
 
 model.save('model')
